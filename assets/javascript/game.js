bdb65cdd4e0f934292f8c95bd405f447096441ea
Code-SY <syanina@gmail.com>
2019-05-31 21:34:37 -0700

    Added pictures, reset button, updated js logic

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -9,45 +9,54 @@ var words = [
     }
 ];
 
+var imagesBasePath = "assets/images/";
+
 var game = {
     winsCount: 0,
-    guessesRemainingCount: 10,
+    guessesRemainingCount: 8,
+    guessesCount: 8,
     word: "",
+    desc: "",
     dummy: "",
     wordLettersCount: 0,
     wrongLetters: [],
     
+    helpElement: null,
     wordElement: null,
     wrongLettersElement: null,
     winsCountElement: null,
     guessesRemainingElement: null,
+    imageElement: null,
 
     init: function() {
         var me = this;
 
         me.winsCount = 0;
 
+        me.helpElement = document.getElementById("help");
         me.wordElement = document.getElementById("word");
         me.wrongLettersElement = document.getElementById("wrongLetters");
         me.winsCountElement = document.getElementById("winsCount");
         me.guessesRemainingElement = document.getElementById("guessesRemainingCount");
+        me.imageElement = document.getElementById("picture");
     },
 
     reset: function() {
         var me = this;
 
-        me.guessesRemainingCount = 10;
+        me.guessesRemainingCount = me.guessesCount;
         me.word = "";
         me.dummy = "";
         me.wordLettersCount = 0;
         me.wrongLetters = [];
     },
 
-    startNew: function(newWord) {
+    startNew: function(newWord, newDesc) {
         var me = this;
 
         me.reset();
 
+        me.desc = newDesc;
         me.word = newWord;
         me.wordLettersCount = me.word.length;
         for (var i = 0; i < me.word.length; i++) {
@@ -59,6 +68,10 @@ var game = {
 
     guessLetter: function(letter) {
         var me = this;
+
+        if (me.guessesRemainingCount <= 0) {
+            return true;
+        }
 
         // Check if letter is in wrongLetters
         if (me.wrongLetters.length > 0) {
@@ -111,25 +124,43 @@ var game = {
     update: function() {
         var me = this;
 
+        me.helpElement.innerHTML = me.desc;
         me.wordElement.innerHTML = me.dummy;
         me.wrongLettersElement.innerHTML = me.wrongLetters.join(",");
         me.winsCountElement.innerHTML = me.winsCount;
         me.guessesRemainingElement.innerHTML = me.guessesRemainingCount;
+
+        if (me.guessesRemainingCount === me.guessesCount) {
+            me.imageElement.src = imagesBasePath + "picClear.jpeg";
+        }
+        else {
+            var imageUrl = imagesBasePath + "pic" + (me.guessesCount - me.guessesRemainingCount) + ".jpeg";
+            me.imageElement.src = imageUrl;
+        }
     }
 };
 
 var currentWordIndex = 0;
 var isDoneWithCurrentWord = false;
 
-game.init();
-game.startNew(words[currentWordIndex++].word);
+var init = function() {
+    currentWordIndex = 0;
+    isDoneWithCurrentWord = false;
+    
+    game.init();
+    game.startNew(words[currentWordIndex].word, words[currentWordIndex].desc);
+    currentWordIndex++;
+}
+
+init();
 
 document.onkeyup = function(event) {
     var key = event.key.toLowerCase();
     
     if (isDoneWithCurrentWord === true) {
         if (key === " " && currentWordIndex < words.length) {
-            game.startNew(words[currentWordIndex++].word);
+            game.startNew(words[currentWordIndex].word, words[currentWordIndex].desc);
+            currentWordIndex++;
             isDoneWithCurrentWord = false;
         }
         return;
@@ -142,3 +173,7 @@ document.onkeyup = function(event) {
 
     isDoneWithCurrentWord = game.guessLetter(key);
 };
+
+onResetClick = function() {
+    init();
+}
